f45e7726abc59dcb2f730527497c73dfcf329b29
Marcin Wałachowski <s19541@pjwstk.edu.pl>
2020-06-23 15:13:07 +0200

    Cw7

---
+++ Cwiczenia7/Cwiczenia7/Controllers/StudentsController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Cwiczenia7.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Cwiczenia7.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        public IConfiguration Configuration { get; set; }
+        public StudentsController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        [HttpGet]
+        [Authorize(Roles = "employee")]
+        public IActionResult GetStudents()
+        {
+            
+            var list = new List<Student>();
+            list.Add(new Student
+            {
+                IndexNumber = 1,
+                FirstName = "Jan",
+                LastName = "Kowalski"
+            });
+            list.Add(new Student
+            {
+                IndexNumber = 2,
+                FirstName = "Jakub",
+                LastName = "Nowak"
+            });
+
+            return Ok(list);
+        }
+        [HttpPost]
+        public IActionResult Login(DTOs.LoginRequestDto request)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, "1"),
+                new Claim(ClaimTypes.Name, "jan123"),
+                new Claim(ClaimTypes.Role, "employee")
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["SecretKey"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken
+            (
+                issuer: "Gakko",
+                audience: "Students",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(10),
+                signingCredentials: creds
+            );
+
+            Student student = new Student();
+            var connection = new SqlConnection("Data Source=db-mssql;Initial Catalog=s19541;Integrated Security=True");
+            connection.Open();
+            using (var com = new SqlCommand())
+            {
+                com.Connection = connection;
+                com.CommandText = "select * from Student where IndexNumber=@login";
+                com.Parameters.AddWithValue("login", request.Login);
+
+                var dr = com.ExecuteReader();
+
+                if (!dr.HasRows)
+                    return BadRequest("Wrong login(index)");
+
+
+            }
+            connection.Close();
+
+            var pass = Configuration["Password"+request.Login];
+            if (request.Password != pass)
+                return BadRequest("Wrong password");
+
+            var refreshToken = Guid.NewGuid();
+            Configuration["RefreshToken"] = refreshToken.ToString();
+            return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token),
+                refreshToken = refreshToken
+            });
+            
+        }
+        [HttpPost("refresh-token/{refreshToken}")]
+        public IActionResult CreateNewToken(string refreshToken)
+        {
+            if (Configuration["RefreshToken"] != refreshToken)
+                return BadRequest("Wrong refreshToken"+ Configuration["RefreshToken"]+"/"+refreshToken);
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, "1"),
+                new Claim(ClaimTypes.Name, "jan123"),
+                new Claim(ClaimTypes.Role, "employee")
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["SecretKey"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken
+            (
+                issuer: "Gakko",
+                audience: "Students",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(10),
+                signingCredentials: creds
+            );
+
+            var refreshTokenNew = Guid.NewGuid();
+            Configuration["RefreshToken"] = refreshTokenNew.ToString();
+            return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token),
+                refreshToken = refreshTokenNew
+            });
+        }
+    }
+}
---
+++ Cwiczenia7/Cwiczenia7/Handles/AuthHandler.cs
@@ -0,0 +1,75 @@
+﻿using Cwiczenia7.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace Cwiczenia7.Handles
+{
+    public class AuthHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        public AuthHandler(
+            IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock
+            ) : base(options, logger, encoder, clock)
+        {
+
+        }
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            if (!Request.Headers.ContainsKey("Authorization"))
+                return AuthenticateResult.Fail("Missing authorization header");
+            var authHeader = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+            var credentialsBytes = Convert.FromBase64String(authHeader.Parameter);
+            var credentials = Encoding.UTF8.GetString(credentialsBytes).Split(":");
+
+            if (credentials.Length != 2)
+                return AuthenticateResult.Fail("Incorrect authorization header value");
+            Student student = new Student();
+            var connection = new SqlConnection("Data Source=db-mssql;Initial Catalog=s19541;Integrated Security=True");
+            connection.Open();
+            using (var com = new SqlCommand())
+            {
+                com.Connection = connection;
+                com.CommandText = "select * from Student where IndexNumber=@login AND Password=@password";
+                com.Parameters.AddWithValue("login", credentials[0]);
+                com.Parameters.AddWithValue("password", credentials[1]);
+
+                var dr = com.ExecuteReader();
+                
+                while (dr.Read())
+                {
+                    student.IndexNumber = Int32.Parse(dr["IndexNumber"].ToString());
+                    student.FirstName = dr["FirstName"].ToString();
+                    student.LastName = dr["LastName"].ToString(); 
+                    student.Password = dr["Password"].ToString();
+                }
+             
+
+            }
+            connection.Close();
+            var claims = new[]
+           {
+                new Claim(ClaimTypes.NameIdentifier, "1"),
+                new Claim(ClaimTypes.Name, credentials[0]),
+            };
+
+            var identity = new ClaimsIdentity(claims, Scheme.Name); 
+            var principal = new ClaimsPrincipal(identity);
+            var ticket = new AuthenticationTicket(principal, Scheme.Name);
+            if (student.FirstName != null)
+                return AuthenticateResult.Success(ticket);
+            return AuthenticateResult.Fail("Bad Login or Password");
+        }
+    }
+}
